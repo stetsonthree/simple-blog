1a9f522e6009489d23165e99d23ce978ba60c0a2
Lee Stetson <stetsonthree@icloud.com>
2020-07-25 22:06:24 -0400

    Adds action_text for rich text editing
    - Removes content from elements
    - Installs action_text
    - Adds images
    - Deletes right text and images
    - Adds header image

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,27 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
+
+import '../stylesheets/application';
+
+document.addEventListener('turbolinks:load', () => {
+  document.addEventListener('click', () => {
+    let element = event.target.closest('.paragraph-content')
+    if (!element) return;
+
+    element.classList.add('d-none')
+    element.nextElementSibling.classList.remove('d-none')
+  })
+
+  document.addEventListener('click', () => {
+    if (!event.target.matches('.cancel')) return;
+
+    let element = event.target.closest('.paragraph-form')
+
+    element.classList.add('d-none')
+    element.previousElementSibling.classList.remove('d-none')
+  })
+})
